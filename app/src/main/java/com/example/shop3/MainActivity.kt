64e1e616d313fba975f3806d0a780f382ccabcc6
Will Lo <darthhun54@gmail.com>
2020-01-01 22:06:47 +0800

    works fine so far

--- app/src/main/java/com/example/shop3/MainActivity.kt
+++ app/src/main/java/com/example/shop3/MainActivity.kt
@@ -1,5 +1,4 @@
 package com.example.shop3
-
 import android.os.Bundle
 import android.util.Log
 import com.google.android.material.snackbar.Snackbar
@@ -16,6 +15,9 @@ class MainActivity : AppCompatActivity() {
 
     private  lateinit var  auth: FirebaseAuth
 
+
+    var TAG = "WillTest"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -26,12 +28,11 @@ class MainActivity : AppCompatActivity() {
                 .setAction("Action", null).show()
         }
 
+        //創建資料到database
 
         val db = FirebaseFirestore.getInstance()
 
         auth = FirebaseAuth.getInstance()
-
-
 
 
         val user = hashMapOf(
@@ -39,12 +40,11 @@ class MainActivity : AppCompatActivity() {
             "first" to "Ada",
             "last" to "Lovelace",
             "born" to 1815,
-            "email" to "darthhun@gmail.com"
-
+            "email" to "darthhun455@gmail.com"
         )
 
         db.collection("users")
-            .document("darthhun@gmail.com")
+            .document("darthhun455@gmail.com")
             .set(user)
             .addOnSuccessListener { documentReference ->
                 Log.d("MainActivity", "DocumentSnapshot added with ID: ${documentReference}")
@@ -53,15 +53,59 @@ class MainActivity : AppCompatActivity() {
                 Log.w("MainActivity", "Error adding document", e)
             }
 
+        //創建資料到database
+
+
+
 
 
     }
+
+    //發文
+
+    private fun articlePost(
+        content: String,
+        id: String,
+        tag: String,
+        title: String,
+        author: String,
+        time: String
+    ) {
+        val db = FirebaseFirestore.getInstance()
+        val docID = db.collection("Articles").document().id
+        val user = hashMapOf(
+            "article_content" to content,
+            "article_id" to docID,
+            "article_tag" to tag,
+            "article_title" to title,
+            "author" to author,
+            "create_time" to time
+        )
+      // Add a new document with a generated ID
+
+        db.collection("Articles")
+            .document(docID)
+            .set(user)
+            .addOnSuccessListener { documentReference ->
+                Log.d(TAG, "DocumentSnapshot added with content_POST:$content")
+            }
+            .addOnFailureListener { e ->
+                Log.w(TAG, "Error adding document_POST", e)
+            }
+    }
+
+
+    //發文
+
+
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
         menuInflater.inflate(R.menu.menu_main, menu)
         return true
     }
+
+
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         // Handle action bar item clicks here. The action bar will
